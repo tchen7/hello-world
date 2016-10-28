52d77404e72df54f112a9ec459b1ce8c63e0387b
tong chen <tchen0322@hotmail.com>
2016-10-28 11:44:25 +0800

    test native git

--- src/com/tchen7/helloworld/HelloWorld.java
+++ src/com/tchen7/helloworld/HelloWorld.java
@@ -1,10 +1,11 @@
 package com.tchen7.helloworld;
 
 /**
+ * Hello world≤‚ ‘
  * Created by tong on 2016/10/28.
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World one more time!");
     }
 }
