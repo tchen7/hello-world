cd987bab2ee5b8b348d5e1d1cda104e75e1f7c1f
tong chen <tchen0322@hotmail.com>
2016-10-28 11:30:02 +0800

    Initial commit

---
+++ src/com/tchen7/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tchen7.helloworld;
+
+/**
+ * Created by tong on 2016/10/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
